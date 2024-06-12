effb456e4369d4c3508b7a0b2e682c65146e7def
baxromxoja16 <aglamof@mail.ru>
2024-06-12 17:20:51 +0500

    feat: added login lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'todo', pathMatch: 'full',
+  },
+  {
+    path: '', loadComponent:() => import('./auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: '**', redirectTo: 'login', pathMatch: 'full',
+  },
+];
