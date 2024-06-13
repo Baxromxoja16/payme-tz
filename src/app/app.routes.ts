55d6600e3d56d01f84ba8c318e7e4250bcc9da42
baxromxoja16 <aglamof@mail.ru>
2024-06-13 09:18:54 +0500

    fix: router redirect path changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '', redirectTo: 'todo', pathMatch: 'full',
+    path: '', redirectTo: 'auth', pathMatch: 'full',
   },
   {
-    path: '', loadComponent:() => import('./auth/login/login.component').then((m) => m.LoginComponent),
+    path: 'auth', loadComponent:() => import('./auth/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: '**', redirectTo: 'login', pathMatch: 'full',
