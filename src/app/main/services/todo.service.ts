5ef64c6b46997dc6537bd02ed3ab51c5c13d0ec3
baxromxoja16 <aglamof@mail.ru>
2024-06-13 11:44:33 +0500

    feat: added todo service

---
+++ src/app/main/services/todo.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ListOfUsers, UserModel } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  
+
+}
