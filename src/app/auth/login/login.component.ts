3cedeb4568494fccc643ce6eb0a57e53a89f587e
baxromxoja16 <aglamof@mail.ru>
2024-06-13 09:52:07 +0500

    feat: added login forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { MaterialsModule } from '../../core/materials.module';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MaterialsModule],
+  imports: [MaterialsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  title = 'title'
-  public isAuthenticated = false;
+  loginForm!: FormGroup;
+  emailRegex = new RegExp(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)
+  errorMessage = '';
+  subscription: Subscription = new Subscription()
 
-  public logout(): void {
-    // todo
+  constructor(
+    private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      "email": ['', [Validators.required, Validators.pattern(this.emailRegex)]],
+      "password": ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 }
