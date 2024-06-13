2fd438911c2c24d73443e97823e0f463014abf07
baxromxoja16 <aglamof@mail.ru>
2024-06-13 11:48:01 +0500

    feat: added error handling logic

--- src/app/core/services/error-handling.service.ts
+++ src/app/core/services/error-handling.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlingService {
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
+
+  handleError(error: any) {
+
+    let errorMessage = '';
+
+    if (error.error instanceof ErrorEvent) {
+
+      // client-side error
+
+      errorMessage = `Error: ${error.error.message}`
+
+    } else {
+
+      // server-side error
+
+      errorMessage = `Error Code: ${error.status} Message: ${error.message}`;
+
+    }
+
+    this._snackBar.open(errorMessage);
+
+    return throwError(errorMessage);
+
+  }
 }
