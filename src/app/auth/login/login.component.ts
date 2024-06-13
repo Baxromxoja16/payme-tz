9acf671a9f4d86d24deb55e3ad280356410bcafe
baxromxoja16 <aglamof@mail.ru>
2024-06-13 09:33:45 +0500

    feat: added login layout

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MaterialsModule } from '../../core/materials.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MaterialsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  title = 'title'
+  public isAuthenticated = false;
 
+  public logout(): void {
+    // todo
+  }
 }
