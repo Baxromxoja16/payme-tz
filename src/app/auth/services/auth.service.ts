7cd444f934cc76ce72400567b07cd4f5e221f4ec
baxromxoja16 <aglamof@mail.ru>
2024-06-13 10:00:02 +0500

    feat: added login logic

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ResponseToken, User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl = '/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(data: User) {
+    return this.http.post<ResponseToken>(this.baseUrl + 'auth/token/login/', data)
+  }
 }
