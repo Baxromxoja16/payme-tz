ad064cbeca6cadbfe2f32fbd270f4103ff9656ad
baxromxoja16 <aglamof@mail.ru>
2024-06-13 11:18:21 +0500

    fix: login guard logic fixed

--- src/app/auth/guards/login.guard.ts
+++ src/app/auth/guards/login.guard.ts
@@ -2,16 +2,11 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const loginGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router);
   const token = localStorage.getItem('token');
   if (token) {
-    router.navigate(['/main']);
-
     return true;
   }
   else {
-    router.navigate(['/login']);
-
     return false;
   }
 };
