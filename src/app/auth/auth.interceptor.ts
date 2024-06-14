17f2a2443edae051fd893f981d04525f3a24e968
baxromxoja16 <aglamof@mail.ru>
2024-06-14 09:42:14 +0500

    fix: auth interceptor changed

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,27 +1,14 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    const cloned = req.clone({
+      headers: req.headers.set("Authorization", `Token ${token}`)
+      .set('Access-Control-Allow-Origin', '*')
+    });
 
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const cloned = request.clone({
-        headers: request.headers.set("Authorization", `Token ${token}`)
-        .set('Access-Control-Allow-Origin', '*')
-      });
-
-      return next.handle(cloned);
-    }
-    return next.handle(request);
+    return next(cloned);
   }
-}
+  return next(req);
+};
