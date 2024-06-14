734123947d7ab8fdf5d4916357c6461d0ad2e660
baxromxoja16 <aglamof@mail.ru>
2024-06-14 09:59:39 +0500

    fix: login guard changed

--- src/app/auth/guards/login.guard.ts
+++ src/app/auth/guards/login.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const loginGuard: CanActivateFn = (route, state) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token')!;
   if (token) {
     return true;
   }
