814263b9fd1359d84fc4267d50f6240e2e0dea4c
baxromxoja16 <aglamof@mail.ru>
2024-06-14 17:32:12 +0500

    fix: auth service handle error

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ResponseToken, User } from '../model/user.model';
+import { catchError } from 'rxjs';
+import { ErrorHandlingService } from '../../core/services/error-handling.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,11 @@ import { ResponseToken, User } from '../model/user.model';
 export class AuthService {
   baseUrl = '/api/';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private errorHandlingService: ErrorHandlingService) {}
 
   login(data: User) {
-    return this.http.post<ResponseToken>(this.baseUrl + 'auth/token/login/', data,  {
-      headers: {'Access-Control-Allow-Origin': '*'}
-    })
+    return this.http.post<ResponseToken>(this.baseUrl + 'auth/token/login/', data).pipe(
+      catchError((err) => this.errorHandlingService.handleError(err))
+    )
   }
 }
